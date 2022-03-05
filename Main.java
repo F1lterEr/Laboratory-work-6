9f8926a6384f062ed7edb6bf2d92fc9e59094a64
F1lterEr <41ilter.er@gmail.com>
2022-03-05 10:04:31 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+class XY{
+    void Sum(double x, double y){
+        double z;
+        z = (x+y)/(y+1);
+        System.out.println("Z = " + z);
+    }
+}
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выполняет выражение: Z = (x+y)/(y+1)");
+        System.out.println("Введите число X: ");
+        double x = scanner.nextDouble();
+        System.out.println("Введите число Y: ");
+        double y = scanner.nextDouble();
+        XY dn = new XY();
+        dn.Sum(x, y);
+    }
+}
